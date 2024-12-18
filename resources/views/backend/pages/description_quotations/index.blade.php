326305ac0464379ba2d75dfaa3f6bd4b0b417fae
dheafareza <mhmmdalfitoo@gmail.com>
2024-12-18 23:38:34 +0700

    bug fix

--- resources/views/backend/pages/description_quotations/index.blade.php
+++ resources/views/backend/pages/description_quotations/index.blade.php
@@ -10,12 +10,11 @@ Description Quotations - Admin Panel
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12 rt-mb-25">
-                    <div class="breadcrumbs ">
-                        <div class="breadcrumb-title"> Description Quotation Management</div>
+                    <div class="breadcrumbs">
+                        <div class="breadcrumb-title"> Description Quotations</div>
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href='/admin'>Home</a></li>
-                                <li class="breadcrumb-item"><a href='/admin'>Setting</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">Description Quotations</li>
                             </ol>
                         </nav>
@@ -29,27 +28,27 @@ Description Quotations - Admin Panel
                             <div class="col-lg-12">
                                 <div class="table-wrapper">
                                     <div class="table-content table-responsive">
-                                        <table class="table align-middle table-basic ">
+                                        <table class="table align-middle table-basic">
                                             <thead style="text-align: center">
                                                 <tr>
                                                     <th scope="col">NO</th>
-                                                    <th scope="col">Description Quotation</th>
-                                                    <th scope="col">Text</th>
-                                                    <th scope="col">Code</th>
+                                                    <th scope="col">TITLE</th>
+                                                    <th scope="col">TEXT</th>
+                                                    <th scope="col">CODE</th>
                                                     <th scope="col">Action</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                                @foreach ($description_quotations as $description_quotation)
+                                                @foreach ($description_quotations as $description)
                                                     <tr>
-                                                        <td scope="row">{{ $loop->index+1 }}</td>
-                                                        <td>{{ $description_quotation->template_inquiry_desc_title }}</td>
-                                                        <td>{{ $description_quotation->template_inquiry_desc_text }}</td>
-                                                        <td>{{ $description_quotation->template_inquiry_desc_code }}</td>
+                                                        <td scope="row">{{ $loop->index + 1 }}</td>
+                                                        <td>{{ $description->template_inquiry_desc_title }}</td>
+                                                        <td>{{ $description->template_inquiry_desc_text }}</td>
+                                                        <td>{{ $description->template_inquiry_desc_code }}</td>
                                                         <td>
                                                             <ul class="action-btn">
                                                                 <li>
-                                                                    <button onclick="delete_data('{{ $description_quotation->template_inquiry_desc_id }}')">
+                                                                    <button onclick="delete_data('{{ $description->template_inquiry_desc_id }}')">
                                                                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                             <path d="M12.5 3.5L3.5 12.5" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
                                                                             <path d="M12.5 12.5L3.5 3.5" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
@@ -57,7 +56,7 @@ Description Quotations - Admin Panel
                                                                     </button>
                                                                 </li>
                                                                 <li>
-                                                                    <button title="Edit" onclick="showedit('{{$description_quotation->template_inquiry_desc_id }}')">
+                                                                    <button title="Edit" onclick="showedit('{{ $description->template_inquiry_desc_id }}')">
                                                                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                             <path d="M12.1464 1.85355C12.3417 1.65829 12.6583 1.65829 12.8536 1.85355L14.1464 3.14645C14.3417 3.34171 14.3417 3.65829 14.1464 3.85355L5.35355 12.6464L2.5 13.5L3.35355 10.6464L12.1464 1.85355Z" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
                                                                             <path d="M11.5 2.5L13.5 4.5" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
@@ -81,14 +80,14 @@ Description Quotations - Admin Panel
                         <div class="card-header">Form Input</div>
                         <div class="card-body">
                             <form>
-                                <input type="hidden" id="template_inquiry_desc_code">
+                                <input type="hidden" id="template_inquiry_desc_id">
                                 <div class="fromGroup mb-3">
-                                    <label>Description Quotation</label>
-                                    <input class="form-control" type="text" id="template_inquiry_desc_title" placeholder="Enter Description Quotation" />
+                                    <label>Title</label>
+                                    <input class="form-control" type="text" id="template_inquiry_desc_title" placeholder="Title" />
                                 </div>
                                 <div class="fromGroup mb-3">
                                     <label>Text</label>
-                                    <textarea class="form-control" name="template_inquiry_desc_text" id="template_inquiry_desc_text" placeholder="Enter Description Text"></textarea>
+                                    <textarea class="form-control" name="template_inquiry_desc_text" id="template_inquiry_desc_text" placeholder="Text"></textarea>
                                 </div>
                                 <div class="row">
                                     <button type="button" class="btn btn-primary pill mt-3" onclick="save()">
@@ -115,15 +114,12 @@ Description Quotations - Admin Panel
 </div>
 
 <script>
-
-    function reload(){
-        // setTimeout(function () {
-            window.open("/admin/description_quotations", "_self");
-        // }, 500);
+    function reload() {
+        window.open("/admin/description_quotations", "_self");
     }
 
     function save() {
-        id = document.getElementById('template_inquiry_desc_code').value;
+        id = document.getElementById('template_inquiry_desc_id').value;
         if (id == '') {
             saveInput();
         } else {
@@ -133,48 +129,40 @@ Description Quotations - Admin Panel
 
     function saveInput() {
         var postdata = new FormData();
-        // Tambahkan token CSRF
         postdata.append('_token', document.getElementsByName('_token')[0].defaultValue);
         postdata.append('template_inquiry_desc_title', document.getElementById('template_inquiry_desc_title').value);
-        postdata.append('template_inquiry_desc_text', document.getElementById('template_inquiry_desc_text').value); 
+        postdata.append('template_inquiry_desc_text', document.getElementById('template_inquiry_desc_text').value);
 
         $.ajax({
             type: "POST",
             url: "/admin/description_quotations",
-            data: (postdata),
-            processData: false, 
+            data: postdata,
+            processData: false,
             contentType: false,
             dataType: "json",
             async: false,
             success: function (data) {
                 if (data.status == 401) {
-                    alert('Form Wajib Harus diisi');
-                    return;
-                } else if (data.status == 501) {
-                    alert(data.message);
-                    return;
+                    alert('Form fields are required');
                 } else {
-                    alert('Berhasil Disimpan');
-                    setTimeout(function () {
-                        window.open("/admin/description_quotations", "_self");
-                    }, 500);
+                    alert('Saved successfully');
+                    reload();
                 }
             },
             error: function (dataerror) {
                 console.log(dataerror);
             }
         });
-
     }
 
-    function showedit(id){
+    function showedit(id) {
         $.ajax({
             type: "GET",
-            url: "/admin/description_quotations/"+id,
+            url: "/admin/description_quotations/" + id,
             dataType: "json",
             async: false,
             success: function (data) {
-                // document.getElementById('template_inquiry_desc_code').value = data.template_inquiry_desc_code;
+                document.getElementById('template_inquiry_desc_id').value = data.template_inquiry_desc_id;
                 document.getElementById('template_inquiry_desc_title').value = data.template_inquiry_desc_title;
                 document.getElementById('template_inquiry_desc_text').value = data.template_inquiry_desc_text;
             },
@@ -186,67 +174,49 @@ Description Quotations - Admin Panel
 
     function updateInput(id) {
         var postdata = new FormData();
-        // Tambahkan token CSRF
         postdata.append('_token', document.getElementsByName('_token')[0].defaultValue);
         postdata.append('template_inquiry_desc_title', document.getElementById('template_inquiry_desc_title').value);
-        postdata.append('template_inquiry_desc_text', document.getElementById('template_inquiry_desc_text').value); 
-    
+        postdata.append('template_inquiry_desc_text', document.getElementById('template_inquiry_desc_text').value);
+
         $.ajax({
-            headers: {
-                'X-CSRF-TOKEN': '{{ csrf_token() }}'
-            },
             type: "POST",
-            url: "/admin/description_quotations/"+id,
-            data: (postdata),
-            processData: false, 
-            contentType: false, 
+            url: "/admin/description_quotations/" + id,
+            data: postdata,
+            processData: false,
+            contentType: false,
             dataType: "json",
             async: false,
             success: function (data) {
-                // console.log('hasil => ',data);
-                
                 if (data.status == 401) {
-                    alert('Form Wajib Harus diisi');
-                    return;
-                } else if (data.status == 501) {
-                    alert(data.message);
-                    return;
+                    alert('Form fields are required');
                 } else {
-                    alert('Berhasil Diupdate');
-                    setTimeout(function () {
-                        window.open("/admin/description_quotations", "_self");
-                    }, 500);
+                    alert('Updated successfully');
+                    reload();
                 }
             },
             error: function (dataerror) {
                 console.log(dataerror);
             }
         });
-
     }
 
-    function delete_data(id){
-        var postdata = {};
-        postdata._token = document.getElementsByName('_token')[0].defaultValue;
-        
+    function delete_data(id) {
+        var postdata = {
+            _token: document.getElementsByName('_token')[0].defaultValue
+        };
+
         $.ajax({
             type: "DELETE",
-            url: "/admin/description_quotations/"+id,
-            data: (postdata),
+            url: "/admin/description_quotations/" + id,
+            data: postdata,
             dataType: "json",
             async: false,
             success: function (data) {
                 if (data.status == 401) {
-                    alert('Form Wajib Harus diisi');
-                    return;
-                } else if (data.status == 501) {
-                    alert(data.message);
-                    return;
+                    alert('Form fields are required');
                 } else {
-                    alert('Data Berhasil Dihapus');
-                    setTimeout(function () {
-                        window.open("/admin/description_quotations", "_self");
-                    }, 500);
+                    alert('Deleted successfully');
+                    reload();
                 }
             },
             error: function (dataerror) {
@@ -254,6 +224,5 @@ Description Quotations - Admin Panel
             }
         });
     }
-
 </script>
 @endsection
