4ceb6104447f9c108a6109a1fd2f8913813607ca
alpitou <mhmmdalfitoo@gmail.com>
2024-12-18 23:26:20 +0700

    [upadate]bug-fix-update-inquiry-goods

--- resources/views/backend/pages/inquiry_goods/index.blade.php
+++ resources/views/backend/pages/inquiry_goods/index.blade.php
@@ -11,12 +11,11 @@ Inquiry Goods - Admin Panel
             <div class="row">
                 <div class="col-12 rt-mb-25">
                     <div class="breadcrumbs">
-                        <div class="breadcrumb-title"> Inquiry Goods Management</div>
+                        <div class="breadcrumb-title"> Inquiry Goods</div>
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href='/admin'>Home</a></li>
-                                <li class="breadcrumb-item"><a href='/admin'>Setting</a></li>
-                                <li class="breadcrumb-item active" aria-current="page"> Inquiry Goods</li>
+                                <li class="breadcrumb-item active" aria-current="page">Inquiry Goods</li>
                             </ol>
                         </nav>
                     </div>
@@ -36,23 +35,21 @@ Inquiry Goods - Admin Panel
                                                     <th scope="col">NAME</th>
                                                     <th scope="col">NOTE</th>
                                                     <th scope="col">CODE</th>
-                                                    <!-- <th scope="col">STATUS</th> -->
                                                     <th scope="col">Action</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
                                                 @foreach ($inquiry_goods as $inquiry_good)
                                                     <tr>
-                                                        <td scope="row">{{ $loop->index+1 }}</td>
+                                                        <td scope="row">{{ $loop->index + 1 }}</td>
                                                         <td>{{ $inquiry_good->inquiry_goods_status_name }}</td>
                                                         <td>{{ $inquiry_good->inquiry_goods_status_notes }}</td>
                                                         <td>{{ $inquiry_good->inquiry_goods_status_code }}</td>
-                                                        <!-- <td>{{ $inquiry_good->inquiry_goods_status_status }}</td> -->
-                                                        <td> 
+                                                        <td>
                                                             <ul class="action-btn">
                                                                 <li>
                                                                     <button onclick="delete_data('{{ $inquiry_good->inquiry_goods_status_id }}')">
-                                                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                        <svg width="16" height="16" fill="none">
                                                                             <path d="M12.5 3.5L3.5 12.5" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
                                                                             <path d="M12.5 12.5L3.5 3.5" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
                                                                         </svg>
@@ -60,7 +57,7 @@ Inquiry Goods - Admin Panel
                                                                 </li>
                                                                 <li>
                                                                     <button title="Edit" onclick="showedit('{{ $inquiry_good->inquiry_goods_status_id }}')">
-                                                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                        <svg width="16" height="16" fill="none">
                                                                             <path d="M12.1464 1.85355C12.3417 1.65829 12.6583 1.65829 12.8536 1.85355L14.1464 3.14645C14.3417 3.34171 14.3417 3.65829 14.1464 3.85355L5.35355 12.6464L2.5 13.5L3.35355 10.6464L12.1464 1.85355Z" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
                                                                             <path d="M11.5 2.5L13.5 4.5" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
                                                                         </svg>
@@ -73,9 +70,6 @@ Inquiry Goods - Admin Panel
                                             </tbody>
                                         </table>
                                     </div>
-                                    <div class="pagination-wrapper">
-                                        {{ $inquiry_goods->links() }}
-                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -86,29 +80,24 @@ Inquiry Goods - Admin Panel
                         <div class="card-header">Form Input</div>
                         <div class="card-body">
                             <form>
-                                <input type="hidden" id="inquiry_goods_id">
+                                <input type="hidden" id="inquiry_good_id">
                                 <div class="fromGroup mb-3">
                                     <label>Name</label>
-                                    <input class="form-control" type="text" id="inquiry_goods_status_name" placeholder="Name" />
+                                    <input class="form-control" type="text" id="inquiry_goods_status_name" placeholder="Inquiry Goods Name" />
                                 </div>
                                 <div class="fromGroup mb-3">
                                     <label>Note</label>
                                     <textarea class="form-control" id="inquiry_goods_status_notes" placeholder="Notes"></textarea>
                                 </div>
-                                <!-- <div class="fromGroup mb-3">
-                                    <label>Status</label>
-                                    <select class="form-control" id="inquiry_goods_status_status">
-                                        <option value="active">Active</option>
-                                        <option value="inactive">Inactive</option>
-                                    </select>
-                                </div> -->
                                 <div class="row">
-                                    <button type="button" class="btn btn-primary pill mt-3" onclick="save()">
+                                    <button type="button" class="btn btn-primary mt-3" onclick="save()">
                                         Save
                                     </button>
-                                    <button type="button" class="btn btn-secondary2 pill btn-icon" onclick="reload()">
+                                    <button type="button" class="btn btn-secondary2 btn-icon" onclick="reload()">
                                         <span class="button-content-wrapper">
-                                            <span class="button-text">New Data</span>
+                                            <span class="button-text">
+                                                New Data
+                                            </span>
                                             <span class="button-icon">
                                                 <i class="ph-arrow-left"></i>
                                             </span>
@@ -123,14 +112,15 @@ Inquiry Goods - Admin Panel
         </div>
     </div>
 </div>
+
 <script>
     function reload() {
         window.open("/admin/inquiry_goods", "_self");
     }
 
     function save() {
-        id = document.getElementById('inquiry_goods_id').value;
-        if (id == '') {
+        let id = document.getElementById('inquiry_good_id').value;
+        if (id === '') {
             saveInput();
         } else {
             updateInput(id);
@@ -138,32 +128,29 @@ Inquiry Goods - Admin Panel
     }
 
     function saveInput() {
-        var postdata = new FormData();
+        let postdata = new FormData();
         postdata.append('_token', document.getElementsByName('_token')[0].defaultValue);
         postdata.append('inquiry_goods_status_name', document.getElementById('inquiry_goods_status_name').value);
         postdata.append('inquiry_goods_status_notes', document.getElementById('inquiry_goods_status_notes').value);
-        // postdata.append('inquiry_goods_status_status', document.getElementById('inquiry_goods_status_status').value);
 
         $.ajax({
             type: "POST",
             url: "/admin/inquiry_goods",
-            data: (postdata),
+            data: postdata,
             processData: false,
             contentType: false,
             dataType: "json",
-            async: false,
-            success: function (data) {
+            success: function(data) {
                 if (data.status === 401) {
-                    alert('Form Wajib Harus diisi');
+                    alert('Form is required');
+                    return;
                 } else {
-                    alert('Berhasil Disimpan');
-                    setTimeout(() => {
-                        reload();
-                    }, 500);
+                    alert('Successfully saved');
+                    setTimeout(() => window.open("/admin/inquiry_goods", "_self"), 500);
                 }
             },
-            error: function (dataerror) {
-                console.error(dataerror);
+            error: function(error) {
+                console.error(error);
             }
         });
     }
@@ -171,83 +158,68 @@ Inquiry Goods - Admin Panel
     function showedit(id) {
         $.ajax({
             type: "GET",
-            url: "/admin/inquiry_goods/"+id,
+            url: "/admin/inquiry_goods/" + id,
             dataType: "json",
-            async: false,
-            success: function (data) {
-                document.getElementById('inquiry_goods_id').value = data.id;
+            success: function(data) {
+                document.getElementById('inquiry_good_id').value = data.inquiry_goods_status_id;
                 document.getElementById('inquiry_goods_status_name').value = data.inquiry_goods_status_name;
                 document.getElementById('inquiry_goods_status_notes').value = data.inquiry_goods_status_notes;
-                // document.getElementById('inquiry_goods_status_status').value = data.inquiry_goods_status_status;
             },
-            error: function (dataerror) {
-                console.error(dataerror);
+            error: function(error) {
+                console.error(error);
             }
         });
     }
 
     function updateInput(id) {
-        var postdata = new FormData();
+        let postdata = new FormData();
         postdata.append('_token', document.getElementsByName('_token')[0].defaultValue);
         postdata.append('inquiry_goods_status_name', document.getElementById('inquiry_goods_status_name').value);
         postdata.append('inquiry_goods_status_notes', document.getElementById('inquiry_goods_status_notes').value);
-        // postdata.append('inquiry_goods_status_status', document.getElementById('inquiry_goods_status_status').value);
 
         $.ajax({
-            headers: {
-                'X-CSRF-TOKEN': '{{ csrf_token() }}'
-            },
             type: "POST",
-            url: "/admin/inquiry_goods/"+id,
-            data: (postdata),
+            url: "/admin/inquiry_goods/" + id,
+            data: postdata,
             processData: false,
             contentType: false,
             dataType: "json",
-            async: false,
-            success: function (data) {
+            success: function(data) {
                 if (data.status === 401) {
-                    alert('Form Wajib Harus diisi');
-                    return;
-                } else if (data.status == 501) {
-                    alert(data.message);
+                    alert('Form is required');
                     return;
                 } else {
-                    alert('Berhasil Diupdate');
-                    setTimeout(() => {
-                        reload();
-                    }, 500);
+                    alert('Successfully updated');
+                    setTimeout(() => window.open("/admin/inquiry_goods", "_self"), 500);
                 }
             },
-            error: function (dataerror) {
-                console.error(dataerror);
+            error: function(error) {
+                console.error(error);
             }
         });
     }
 
     function delete_data(id) {
-        let postdata = {};
-        postdata._token = document.getElementsByName('_token')[0].defaultValue;
+        let postdata = {
+            _token: document.getElementsByName('_token')[0].defaultValue
+        };
 
         $.ajax({
             type: "DELETE",
-            url: `/admin/inquiry_goods/${id}`,
+            url: "/admin/inquiry_goods/" + id,
             data: postdata,
             dataType: "json",
-            async: false,
-            success: function (data) {
+            success: function(data) {
                 if (data.status === 401) {
-                    alert('Operation failed.');
-                } else if (data.status === 501) {
-                    alert(data.message);
+                    alert('Form is required');
+                    return;
                 } else {
-                    alert('Data successfully deleted.');
-                    setTimeout(() => {
-                        reload();
-                    }, 500);
+                    alert('Successfully deleted');
+                    setTimeout(() => window.open("/admin/inquiry_goods", "_self"), 500);
                 }
             },
-            error: function (dataerror) {
-                console.error(dataerror);
+            error: function(error) {
+                console.error(error);
             }
         });
     }
