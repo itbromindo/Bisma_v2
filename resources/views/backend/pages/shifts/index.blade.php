b9c34573d9d328412fa0e18763abd078599cc68b
alpitou <mhmmdalfitoo@gmail.com>
2024-12-21 13:51:15 +0700

    Update index.blade.php in shifts page

--- resources/views/backend/pages/shifts/index.blade.php
+++ resources/views/backend/pages/shifts/index.blade.php
@@ -31,14 +31,17 @@ Shifts - Admin Panel
                                 <div class="app-main-search me-2">
                                     <form action="/admin/shifts" method="GET" class="d-flex">
                                         <div class="input-box d-flex">
-                                            <input type="text" name="search" id="search" value="{{$search ?? ''}}" class="form-control" placeholder="Search Here">
+                                            <input type="text" name="search" id="search" value="{{$search ?? ''}}"
+                                                class="form-control" placeholder="Search Here">
                                             <button type="submit" class="btn btn-light ms-2">
-                                                <img src="{{ asset('backend/assets/images/svg/search.svg')}}" alt="Search" draggable="false">
+                                                <img src="{{ asset('backend/assets/images/svg/search.svg')}}"
+                                                    alt="Search" draggable="false">
                                             </button>
                                         </div>
                                     </form>
                                 </div>
-                                <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalinput">Tambah Data</button>
+                                <button type="button" class="btn btn-success" data-bs-toggle="modal"
+                                    data-bs-target="#modalinput">Tambah Data</button>
                             </div>
                         </div>
 
@@ -77,16 +80,37 @@ Shifts - Admin Panel
                                                                 <td>{{ $shift->shift_notes }}</td>
                                                                 <td class="text-center">
                                                                     <div class="d-flex justify-content-center gap-2">
-                                                                        <button class="btn btn-light btn-sm border border-danger text-danger" title="Delete" onclick="delete_data('{{ $shift->shift_id }}')">
-                                                                            <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                                                <path d="M12.5 3.5L3.5 12.5" stroke="red" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
-                                                                                <path d="M12.5 12.5L3.5 3.5" stroke="red" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                                                        <button
+                                                                            class="btn btn-light btn-sm border border-danger text-danger"
+                                                                            title="Delete"
+                                                                            onclick="delete_data('{{ $shift->shift_id }}')">
+                                                                            <svg width="16" height="16" fill="none"
+                                                                                xmlns="http://www.w3.org/2000/svg">
+                                                                                <path d="M12.5 3.5L3.5 12.5" stroke="red"
+                                                                                    stroke-width="1.25"
+                                                                                    stroke-linecap="round"
+                                                                                    stroke-linejoin="round" />
+                                                                                <path d="M12.5 12.5L3.5 3.5" stroke="red"
+                                                                                    stroke-width="1.25"
+                                                                                    stroke-linecap="round"
+                                                                                    stroke-linejoin="round" />
                                                                             </svg>
                                                                         </button>
-                                                                        <button class="btn btn-light btn-sm border border-success text-success" title="Edit" onclick="showedit('{{ $shift->shift_id }}')">
-                                                                            <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                                                <path d="M12.1464 1.85355C12.3417 1.65829 12.6583 1.65829 12.8536 1.85355L14.1464 3.14645C14.3417 3.34171 14.3417 3.65829 14.1464 3.85355L5.35355 12.6464L2.5 13.5L3.35355 10.6464L12.1464 1.85355Z" stroke="green" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
-                                                                                <path d="M11.5 2.5L13.5 4.5" stroke="green" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                                                        <button
+                                                                            class="btn btn-light btn-sm border border-success text-success"
+                                                                            title="Edit"
+                                                                            onclick="showedit('{{ $shift->shift_id }}')">
+                                                                            <svg width="16" height="16" fill="none"
+                                                                                xmlns="http://www.w3.org/2000/svg">
+                                                                                <path
+                                                                                    d="M12.1464 1.85355C12.3417 1.65829 12.6583 1.65829 12.8536 1.85355L14.1464 3.14645C14.3417 3.34171 14.3417 3.65829 14.1464 3.85355L5.35355 12.6464L2.5 13.5L3.35355 10.6464L12.1464 1.85355Z"
+                                                                                    stroke="green" stroke-width="1.25"
+                                                                                    stroke-linecap="round"
+                                                                                    stroke-linejoin="round" />
+                                                                                <path d="M11.5 2.5L13.5 4.5" stroke="green"
+                                                                                    stroke-width="1.25"
+                                                                                    stroke-linecap="round"
+                                                                                    stroke-linejoin="round" />
                                                                             </svg>
                                                                         </button>
                                                                     </div>
@@ -105,19 +129,25 @@ Shifts - Admin Panel
                                         <nav aria-label="Page navigation">
                                             <ul class="pagination justify-content-end">
                                                 <li class="page-item {{ $shifts->onFirstPage() ? 'disabled' : '' }}">
-                                                    <a class="page-link" href="{{ $shifts->appends(['search' => $search ?? request('search')])->previousPageUrl() }}" aria-label="Previous">
+                                                    <a class="page-link"
+                                                        href="{{ $shifts->appends(['search' => $search ?? request('search')])->previousPageUrl() }}"
+                                                        aria-label="Previous">
                                                         <i class="ph-arrow-left"></i>
                                                     </a>
                                                 </li>
 
                                                 @foreach ($shifts->onEachSide(0)->appends(['search' => $search ?? request('search')])->links()->elements[0] as $page => $url)
-                                                    <li class="page-item {{ $shifts->currentPage() == $page ? 'active' : '' }}">
-                                                        <a class="page-link" href="{{ $url }}">{{ sprintf('%02d', $page) }}</a>
+                                                    <li
+                                                        class="page-item {{ $shifts->currentPage() == $page ? 'active' : '' }}">
+                                                        <a class="page-link"
+                                                            href="{{ $url }}">{{ sprintf('%02d', $page) }}</a>
                                                     </li>
                                                 @endforeach
 
                                                 <li class="page-item {{ $shifts->hasMorePages() ? '' : 'disabled' }}">
-                                                    <a class="page-link" href="{{ $shifts->appends(['search' => $search ?? request('search')])->nextPageUrl() }}" aria-label="Next">
+                                                    <a class="page-link"
+                                                        href="{{ $shifts->appends(['search' => $search ?? request('search')])->nextPageUrl() }}"
+                                                        aria-label="Next">
                                                         <i class="ph-arrow-right"></i>
                                                     </a>
                                                 </li>
@@ -151,23 +181,28 @@ Shifts - Admin Panel
                     </div>
                     <div class="formGroup mb-3">
                         <label>Notes</label>
-                        <textarea class="form-control" name="shift_notes" id="shift_notes" placeholder="Notes"></textarea>
+                        <textarea class="form-control" name="shift_notes" id="shift_notes"
+                            placeholder="Notes"></textarea>
                     </div>
                     <div class="formGroup mb-3">
                         <label>Start Time Before Break</label>
-                        <input class="form-control" type="time" id="shift_start_time_before_break" placeholder="Start Time Before Break" />
+                        <input class="form-control" type="time" id="shift_start_time_before_break"
+                            placeholder="Start Time Before Break" />
                     </div>
                     <div class="formGroup mb-3">
                         <label>End Time Before Break</label>
-                        <input class="form-control" type="time" id="shift_end_time_before_break" placeholder="End Time Before Break" />
+                        <input class="form-control" type="time" id="shift_end_time_before_break"
+                            placeholder="End Time Before Break" />
                     </div>
                     <div class="formGroup mb-3">
                         <label>Start Time After Break</label>
-                        <input class="form-control" type="time" id="shift_start_time_after_break" placeholder="Start Time After Break" />
+                        <input class="form-control" type="time" id="shift_start_time_after_break"
+                            placeholder="Start Time After Break" />
                     </div>
                     <div class="formGroup mb-3">
                         <label>End Time After Break</label>
-                        <input class="form-control" type="time" id="shift_end_time_after_break" placeholder="End Time After Break" />
+                        <input class="form-control" type="time" id="shift_end_time_after_break"
+                            placeholder="End Time After Break" />
                     </div>
                     <select class="form-control mb-3" id="companies_code">
                         <option value="">Select Company</option>
@@ -212,6 +247,11 @@ Shifts - Admin Panel
         id ? updateInput(id) : saveInput();
     }
 
+    function formatTime(time) {
+        const [hour, minute] = time.split(':');
+        return `${hour}:${minute}`;
+    }
+
     function saveInput() {
         const data = {
             shift_name: document.getElementById('shift_name').value,
@@ -224,7 +264,7 @@ Shifts - Admin Panel
             _token: '{{ csrf_token() }}'
         };
 
-        $.post('/admin/shifts', data, function(response) {
+        $.post('/admin/shifts', data, function (response) {
             if (response.status === 401) {
                 Swal.fire({
                     icon: 'error',
@@ -236,7 +276,7 @@ Shifts - Admin Panel
                     icon: 'success',
                     title: 'Success',
                     text: 'Data Saved!',
-                }).then(function() {
+                }).then(function () {
                     location.reload();
                 });
             }
@@ -244,13 +284,18 @@ Shifts - Admin Panel
     }
 
     function updateInput(id) {
+        const shiftStartBeforeBreak = document.getElementById('shift_start_time_before_break').value;
+        const shiftEndBeforeBreak = document.getElementById('shift_end_time_before_break').value;
+        const shiftStartAfterBreak = document.getElementById('shift_start_time_after_break').value;
+        const shiftEndAfterBreak = document.getElementById('shift_end_time_after_break').value;
+
         const data = {
             shift_name: document.getElementById('shift_name').value,
             shift_notes: document.getElementById('shift_notes').value,
-            shift_start_time_before_break: document.getElementById('shift_start_time_before_break').value,
-            shift_end_time_before_break: document.getElementById('shift_end_time_before_break').value,
-            shift_start_time_after_break: document.getElementById('shift_start_time_after_break').value,
-            shift_end_time_after_break: document.getElementById('shift_end_time_after_break').value,
+            shift_start_time_before_break: formatTime(shiftStartBeforeBreak),
+            shift_end_time_before_break: formatTime(shiftEndBeforeBreak),
+            shift_start_time_after_break: formatTime(shiftStartAfterBreak),
+            shift_end_time_after_break: formatTime(shiftEndAfterBreak),
             companies_code: document.getElementById('companies_code').value,
             _token: '{{ csrf_token() }}'
         };
@@ -259,7 +304,7 @@ Shifts - Admin Panel
             url: `/admin/shifts/${id}`,
             type: 'PUT',
             data: data,
-            success: function(response) {
+            success: function (response) {
                 if (response.status === 401) {
                     Swal.fire({
                         icon: 'error',
@@ -271,7 +316,7 @@ Shifts - Admin Panel
                         icon: 'success',
                         title: 'Success',
                         text: 'Data Updated!',
-                    }).then(function() {
+                    }).then(function () {
                         location.reload();
                     });
                 }
@@ -294,12 +339,12 @@ Shifts - Admin Panel
                     url: `/admin/shifts/${id}`,
                     type: 'DELETE',
                     data: { _token: '{{ csrf_token() }}' },
-                    success: function(response) {
+                    success: function (response) {
                         Swal.fire({
                             icon: 'success',
                             title: 'Deleted!',
                             text: 'Data has been deleted.',
-                        }).then(function() {
+                        }).then(function () {
                             location.reload();
                         });
                     }
@@ -309,7 +354,7 @@ Shifts - Admin Panel
     }
 
     function showedit(id) {
-        $.get(`/admin/shifts/${id}`, function(data) {
+        $.get(`/admin/shifts/${id}`, function (data) {
             document.getElementById('shift_id').value = data.shift_id;
             document.getElementById('shift_name').value = data.shift_name;
             document.getElementById('shift_notes').value = data.shift_notes;
