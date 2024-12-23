590fdf8539a021a03eea75ff2739d8dd04e8c7a8
alpitou <mhmmdalfitoo@gmail.com>
2024-12-23 22:54:54 +0700

    Push inquiry_goods_code

--- resources/views/backend/pages/inquiry_goods/index.blade.php
+++ resources/views/backend/pages/inquiry_goods/index.blade.php
@@ -54,6 +54,7 @@ Inquiry Goods - Admin Panel
                                                             <th scope="col" width="5%">NO</th>
                                                             <th scope="col">NAME</th>
                                                             <th scope="col">NOTE</th>
+                                                            <th scope="col">CODE</th>
                                                             <th scope="col">Action</th>
                                                         </tr>
                                                     </thead>
@@ -65,6 +66,7 @@ Inquiry Goods - Admin Panel
                                                                 </td>
                                                                  <td class="text-center">{{ $item->inquiry_goods_status_name }}</td>
                                                                  <td class="text-center">{{ $item->inquiry_goods_status_notes }}</td>
+                                                                 <td class="text-center">{{ $item->inquiry_goods_status_code }}</td>
                                                                 <td class="text-center">
                                                                     <div class="d-flex justify-content-center gap-2">
                                                                         <button class="btn btn-light btn-sm border border-danger text-danger" title="Delete" onclick="delete_data('{{ $item->inquiry_goods_status_id }}')">
@@ -155,6 +157,56 @@ Inquiry Goods - Admin Panel
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script>
+    $(document).ready(function () {
+        $('#search').on('keyup', function () {
+            let searchQuery = $(this).val();
+            $.ajax({
+                url: '/admin/inquiry_goods',
+                type: 'GET',
+                data: { search: searchQuery },
+                success: function (response) {
+
+                    $('#tableBody').html('');
+                    if (response.inquiry_goods.data.length > 0) {
+                        response.inquiry_goods.data.forEach(function (item, index) {
+                            $('#tableBody').append(`
+                                <tr>
+                                    <td class="text-center">${(response.inquiry_goods.current_page - 1) * response.inquiry_goods.per_page + index + 1}</td>
+                                    <td class="text-center">${item.inquiry_goods_status_name}</td>
+                                    <td class="text-center">${item.inquiry_goods_status_notes ?? '-'}</td>
+                                    <td class="text-center">${item.inquiry_goods_status_code}</td>
+                                    <td class="text-center">
+                                                                    <div class="d-flex justify-content-center gap-2">
+                                                                        <button class="btn btn-light btn-sm border border-danger text-danger" title="Delete" onclick="delete_data('${item.inquiry_goods_status_id}')">
+                                                                            <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                                <path d="M12.5 3.5L3.5 12.5" stroke="red" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                                                                <path d="M12.5 12.5L3.5 3.5" stroke="red" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                                                            </svg>
+                                                                        </button>
+                                                                        <button class="btn btn-light btn-sm border border-success text-success" title="Edit" onclick="showedit('${item.inquiry_goods_status_id}')">
+                                                                            <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                                <path d="M12.1464 1.85355C12.3417 1.65829 12.6583 1.65829 12.8536 1.85355L14.1464 3.14645C14.3417 3.34171 14.3417 3.65829 14.1464 3.85355L5.35355 12.6464L2.5 13.5L3.35355 10.6464L12.1464 1.85355Z" stroke="green" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                                                                <path d="M11.5 2.5L13.5 4.5" stroke="green" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                                                            </svg>
+                                                                        </button>
+                                                                    </div>
+                                                                </td>
+                                </tr>
+                            `);
+                        });
+                    } else {
+                        $('#tableBody').append('<tr><td colspan="5" class="text-center">No results found</td></tr>');
+                    }
+                },
+                error: function (xhr) {
+                    alert('Error: ' + xhr.statusText);
+                }
+            });
+        });
+    });
+
+    
+
     function reload() {
         window.open("/admin/inquiry_goods", "_self");
     }
